f8af9cd8720b90403d6ec96e9984beaebeee6af4
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-26 16:56:07 +0600

    google-authenticaation

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,15 +1,23 @@
 import { createContext } from "react";
-
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import auth from "../Compunets/Firbase/Firbase";
 
 const AuthContext  =createContext(null)
-
+const GoogleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
 
 
+    // google Login
+    const googleLogin = () =>{
+       return signInWithPopup(auth, GoogleProvider)
 
-
+      
+    }
+    const authentication ={
+        googleLogin
+  }
     return (
-        <AuthContext.Provider value={}>
+        <AuthContext.Provider value={authentication}>
            {children}
         </AuthContext.Provider>
     
@@ -17,3 +25,6 @@ const AuthProvider = ({children}) => {
 };
 
 export default AuthProvider;
+
+
+
