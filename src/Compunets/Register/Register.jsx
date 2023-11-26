1919c82b67a7f8297b2a641d19032de14f16745e
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-26 16:14:46 +0600

    app-commit

--- src/Compunets/Register/Register.jsx
+++ src/Compunets/Register/Register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Register ogin now!</h1>
+      <h1 className="text-5xl font-bold">Register now!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -26,9 +26,9 @@ const Register = () => {
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Img Url</span>
+            <span className="label-text">IMG URL</span>
           </label>
-          <input type="email" placeholder="Img Url" className="input input-bordered" required />
+          <input type="email" placeholder="img url" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
